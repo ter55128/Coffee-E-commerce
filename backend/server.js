c74c916ca04acf1106fc7691fbc319b1b879d2df
Hank <linkuanhan8811@gmail.com>
2025-04-21 15:04:21 +0800

    sigterm2

--- backend/server.js
+++ backend/server.js
@@ -91,6 +91,8 @@ app.get("/", (req, res) => {
   res.send("API is running");
 });
 
-app.listen(8080, () => {
-  console.log(`Server is listening on port 8080`);
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server is running on port ${PORT}`);
 });
